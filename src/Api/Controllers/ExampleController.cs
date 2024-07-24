315c1c9a18fdd66d1e49250a56374030a616a1d3
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-07-24 21:13:44 +0200

    Add API versioning

--- src/Api/Controllers/ExampleController.cs
+++ src/Api/Controllers/ExampleController.cs
@@ -1,15 +1,25 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
 public class ExampleController(ILogger<ExampleController> logger) : ControllerBase
 {
     [HttpGet]
-    public IEnumerable<object> Get()
+    [ApiVersion("1.0")]
+    public IEnumerable<object> GetV1()
     {
         // TODO: implement this endpoint.
-        return new List<object>();
+        return new List<object> { new() };
+    }
+    
+    [HttpGet]
+    [ApiVersion("2.0")]
+    public IEnumerable<object> GetV2()
+    {
+        // TODO: implement this endpoint.
+        return new List<object> { new(), new() };
     }
 }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add API versioning.
+builder.Services
+    .AddApiVersioning(options =>
+    {
+        // To set the current API version.
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        // To use the default API version if it is not specified.
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        // To send a header with the supported versions.
+        options.ReportApiVersions = true;
+        // To indicate the way to indicate the version.
+        options.ApiVersionReader = new Asp.Versioning.UrlSegmentApiVersionReader();
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
 
 var app = builder.Build();
 
