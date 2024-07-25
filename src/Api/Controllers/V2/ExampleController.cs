aa4d55fe2b86204028a04f3994e5961bef357198
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-07-25 22:52:24 +0200

    Comment ExampleController.cs (v1 and v2)
    This is to avoid showing them in the Swagger doc

--- src/Api/Controllers/V2/ExampleController.cs
+++ src/Api/Controllers/V2/ExampleController.cs
@@ -4,14 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.V2;
 
-[ApiVersion("2.0")]
-public class ExampleController(ILogger<ExampleController> logger) : ApiControllerBase
-{
-    [ApiVersion("2.0")]
-    //[MapToApiVersion("2.0")]
-    [HttpGet]
-    public IEnumerable<object> GetV2()
-    {
-        return new List<object> { new(), new() };
-    }
-}
+// [ApiVersion("2.0")]
+// public class ExampleController(ILogger<ExampleController> logger) : ApiControllerBase
+// {
+//     [ApiVersion("2.0")]
+//     //[MapToApiVersion("2.0")]
+//     [HttpGet]
+//     public IEnumerable<object> GetV2()
+//     {
+//         return new List<object> { new(), new() };
+//     }
+// }
