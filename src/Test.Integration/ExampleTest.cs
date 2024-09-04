04d1137a3e9153914444762e57f8ee04c4c42f37
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-09-04 21:17:02 +0200

    WIP integration tests: delete TODO comment
    Continue with the rest of the TODOs for integration testing to finish this part.

--- src/Test.Integration/ExampleTest.cs
+++ src/Test.Integration/ExampleTest.cs
@@ -11,7 +11,6 @@ public class ExampleTest : IntegrationTest
     [Fact]
     public async Task Do()
     {
-        // TODO: check integration tests youtube video from the playlist and the net core doc.
         var response = Client.GetAsync("api/v1/Transactions");
         var result = await response;
         Assert.Equal(HttpStatusCode.OK, result.StatusCode);
