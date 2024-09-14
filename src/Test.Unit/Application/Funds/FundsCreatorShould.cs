513d9c2823deb474bb2a4ec304c30488318d9a8e
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-09-14 12:46:12 +0200

    WIP: creating unit test for adding fund endpoint

--- src/Test.Unit/Application/Funds/FundsCreatorShould.cs
+++ src/Test.Unit/Application/Funds/FundsCreatorShould.cs
@@ -1,24 +1,40 @@
+using Application.Funds;
+using Domain.Funds.Repositories;
+using Domain.Funds.Services;
+using Test.Unit.Contracts.Funds;
 using Test.Unit.Domain.Funds;
 
 namespace Test.Unit.Application.Funds;
 
 public class FundsCreatorShould
 {
+    private readonly FundFinder _fundFinder;
+    private readonly FundsCreator _fundsCreator;
+    private readonly IFundsRepository _fundsRepository;
+
+    public FundsCreatorShould()
+    {
+        _fundsRepository = Substitute.For<IFundsRepository>();
+        _fundFinder = new FundFinder(_fundsRepository);
+        _fundsCreator = new FundsCreator(_fundsRepository, _fundFinder);
+    }
+
+    // TODO: create a set of date for executing the tests with different possibilities.
     [Fact]
     public void Invoke_WithoutPassingId_CreatesAndReturnsTheFundCreated()
     {
         // Given
-        // TODO: see how to set a value to null without setting it to random value like now with Apply method.
-        // TODO: see if this should be called Mother or Stub.
         var fund = FundMother.Random();
+        var parameters = CreateFundRequestMother.WithoutId();
+
+        _fundsRepository.Create(fund).Returns(fund);
 
         // When
-        // TODO: call to FundsCreator passing the fund.
+        _ = _fundsCreator.Invoke(parameters.Id, parameters.Name, parameters.Description);
 
         // Then
-        // TODO: compare the result got with the expected result.
-        // TODO: create a set of date for executing the tests with different possibilities.
-        fund.Should().Be(fund);
+        // TODO: why this does not work? See if is because of the fund.
+        _fundsRepository.Received().Create(fund);
     }
 
     // TODO: create more test cases like Invoke_PassingId_CreatesAndReturnsTheFundCreated.
