0b2891873a164b3f7c1a61a3c4e3cc7986302b2a
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-10-05 13:23:37 +0200

    docs: add Limit.cs doc
    To understand why it was created and it is not just a  property of Category.cs

--- src/Domain/Limits/Limit.cs
+++ src/Domain/Limits/Limit.cs
@@ -1,5 +1,12 @@
+using Domain.Categories.Models;
+
 namespace Domain.Limits;
 
+/// <summary>
+///     Amount of money that can be spent in a <see cref="Category" /> within a period of time specified.
+///     <br />
+///     For now, it is only available "per month" limit period so there is no property to specify it.
+/// </summary>
 public class Limit
 {
     public Guid Id { get; set; }
